1bb4cf11aab1f28f28385365af82ced2ced25bf2
J.R. Patten <jr@juanpatten.com>
2013-05-02 11:53:12 -0600

    [m] version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'unn',
-    version = '0.0.2',
+    version = '0.0.3',
     packages = ['unn',],
     license = 'MIT',
     long_description = open('README.md').read(),
