1f2b881e4ecfb6da8add9fcb703233f08aec0584
J.R. Patten <jr@juanpatten.com>
2013-05-02 10:58:31 -0600

    Set up for python packaging

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = 'unn',
+    version = '0.0.2',
+    packages = ['unn',],
+    license = 'MIT',
+    long_description = open('README.md').read(),
+)
