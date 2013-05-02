4807058964e17853e5dccc0cbdbfc0c07e19ea11
J.R. Patten <jr@juanpatten.com>
2013-05-02 12:46:01 -0600

    Add executable to package

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from distutils.core import setup
 
 setup(
     name = 'unn',
-    version = '0.0.3',
+    version = '0.0.4',
     packages = ['unn',],
+    scripts = ['unn/unn'],
     license = 'MIT',
     long_description = open('README.md').read(),
 )
