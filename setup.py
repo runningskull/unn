1073a508312780131bf1fcb1c22d6d43784a43aa
J.R. Patten <jr@juanpatten.com>
2013-05-02 12:59:47 -0600

    Add dependencies to setup

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from distutils.core import setup
 
+requires = open('requirements.txt').read().split('\n')
+
 setup(
     name = 'unn',
-    version = '0.0.5',
+    version = '0.0.6',
     packages = ['unn',],
     scripts = ['unn/unn'],
     license = 'MIT',
+    requires = requires,
     long_description = open('README.md').read(),
 )
