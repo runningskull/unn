9564215f6129ca8e96b7b6947c068bf5db848364
J.R. Patten <jr@juanpatten.com>
2013-09-23 21:50:44 -0600

    Bump version for new package description

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ requires = open('requirements.txt').read().split('\n')
 
 setup(
     name = 'unn',
-    version = '0.0.13',
+    version = '0.0.14',
     packages = ['unn',],
     scripts = ['unn/unn'],
     license = 'MIT',
